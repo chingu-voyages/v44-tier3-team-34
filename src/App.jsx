01fe50eedba89a9366a928030fcb1b3bf709e953
GregNicholas <gregschoenberg@gmail.com>
2023-05-17 22:47:11 -0700

    setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components'
 import Landing from './pages/Landing';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
-import GlobalStyle from './globalStyles';
-
-// theme styles can be used in components wrapped in ThemeProvider
-const theme = {
-  colors: {
-    green: "#1EB88D",
-    lightGreen: "#C0E862",
-    darkBlue: "#0D1C2E",
-    white: "#FFFFFF",
-  }
-}
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
       <Router>
           <Routes>
-            <Route path='/' element={<Landing />} />
+            <Route index element={<Landing />} />
             <Route path='/home' element={<Home />} />
             <Route path='/Login' element={<Login />} />
             <Route path='/Signup' element={<Signup />} />
             <Route path='/Profile' element={<Profile />} />
           </Routes>
       </Router>
-      </ThemeProvider>
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const plugin = require('tailwindcss/plugin')
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      'green': '#1EB88D',
+      'light-green': '#C0E862',
+      'light-blue': '#2F4858',
+      'dark-blue': '#0D1C2E',
+      'white': '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'hero': "url('./src/assets/pet-hero.jpg')",
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '2px 2px 4px rgba(0,0,0,0.5)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+}
