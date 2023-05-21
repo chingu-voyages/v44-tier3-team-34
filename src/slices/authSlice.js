1497601903bf4644fc3816b78ebbca0ab6c52074
GregNicholas <gregschoenberg@gmail.com>
2023-05-20 21:14:37 -0700

    set up auth slice for local state

---
+++ src/slices/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.user = action.payload;
+      localStorage.setItem('user', JSON.stringify(action.payload));
+    },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem('user');
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+
+export default authSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: true
 });
