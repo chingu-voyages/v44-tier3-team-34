ed582fa6bae10058e223dcdd3a75b78e932d980e
GregNicholas <gregschoenberg@gmail.com>
2023-06-09 14:13:32 -0700

    add endpoints to add and delete reactions

--- src/slices/postsApiSlice.js
+++ src/slices/postsApiSlice.js
@@ -84,6 +84,27 @@ export const postsApiSlice = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ["Posts"],
     }),
+    addReaction: builder.mutation({
+      query: (reactionData) => ({
+        url: `${POSTS_URL}/${reactionData.postId}/comments`,
+        method: "POST",
+        body: { reaction: reactionData.reaction },
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      }),
+      invalidatesTags: ["Posts"],
+    }),
+    deleteReaction: builder.mutation({
+      query: ({postId, reactionId}) => ({
+        url: `${POSTS_URL}/${postId}/reactions/${reactionId}`,
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      }),
+      invalidatesTags: ["Posts"],
+    }),
   }),
 });
 
